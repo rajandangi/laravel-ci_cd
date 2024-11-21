4d96ee465a732f1a43c3f464f440cded5bcd2d9c
rajandangi <rajandangi649@gmail.com>
2024-11-21 17:55:53 +1030

    CI/CD-02 : Test Laravel Pint Workflow

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,8 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class TestController extends Controller
-{
+class TestController extends Controller{
     /**
      * Display a listing of the resource.
      */
